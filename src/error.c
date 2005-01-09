f7340332edd93575cabbce423b1308781a518683
tsaviran <tsaviran@e7bde6ef-2016-0410-acfd-d3fa7a0704fe>
2005-01-09 19:55:06 +0000

    splitted error and report into error.[ch]
    git-svn-id: https://miau.svn.sourceforge.net/svnroot/miau/trunk@176 e7bde6ef-2016-0410-acfd-d3fa7a0704fe

---
+++ src/error.c
@@ -0,0 +1,93 @@
+/* $Id$
+ * -------------------------------------------------------
+ * Copyright (C) 2003-2005 Tommi Saviranta <tsaviran@cs.helsinki.fi>
+ * -------------------------------------------------------
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <config.h>
+#include "error.h"
+#include "tools.h"
+#ifndef TESTING
+#include "irc.h"
+#endif /* ifndef TESTING */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+
+
+#ifdef ENDUSERDEBUG
+#define ENDUSER_BUF_SIZE	8196
+void
+enduserdebug(
+		char	*format,
+		...
+	    )
+{
+	va_list va;
+	char buf0[ENDUSER_BUF_SIZE];
+
+	va_start(va, format);
+	vsnprintf(buf0, ENDUSER_BUF_SIZE - 1, format, va);
+	va_end(va);
+#ifndef TESTING
+	if (c_clients.connected > 0) {
+		char buf1[ENDUSER_BUF_SIZE + 160];
+		sprintf(buf1, ":debug PRIVMSG %s :%s: %s", status.nickname,
+				get_timestamp(NULL, TIMESTAMP_LONG), buf0);
+		irc_mwrite(&c_clients, "%s", buf1);
+	}
+#endif /* ifndef TESTING */
+} /* void enduserdebug(char *format, ...) */
+#endif /* ifdef ENDUSERDEBUG */
+
+
+
+void
+report(
+		char	*format,
+		...
+      )
+{
+	char	buffer[256];
+	va_list	va;
+	
+	va_start(va, format);
+	vsnprintf(buffer, 255, format, va);
+	va_end(va);
+
+	fprintf(stdout, "%s + %s\n", get_short_localtime(), buffer);
+#ifndef TESTING
+	irc_mnotice(&c_clients, status.nickname, buffer);
+#endif /* ifndef TESTING */
+} /* void report(char *format, ...) */
+
+
+
+void
+error(
+		char	*format,
+		...
+     )
+{
+	char	buffer[256];
+	va_list	va;
+
+	va_start(va, format);
+	vsnprintf(buffer, 255, format, va);
+	va_end(va);
+	
+	fprintf(stdout, "%s - %s\n", get_short_localtime(), buffer);
+#ifndef TESTING
+	irc_mnotice(&c_clients, status.nickname, buffer);
+#endif /* ifndef TESTING */
+} /* void error(char *format, ...) */
