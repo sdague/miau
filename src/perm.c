39e3d600aa5f18e384bd69cf6be0f2fae19af996
tsaviran <tsaviran@e7bde6ef-2016-0410-acfd-d3fa7a0704fe>
2005-01-09 19:47:21 +0000

    uses match_replace
    git-svn-id: https://miau.svn.sourceforge.net/svnroot/miau/trunk@166 e7bde6ef-2016-0410-acfd-d3fa7a0704fe

--- src/perm.c
+++ src/perm.c
@@ -1,6 +1,6 @@
-/*
+/* $Id$
  * -------------------------------------------------------
- * Copyright (C) 2002-2004 Tommi Saviranta <tsaviran@cs.helsinki.fi>
+ * Copyright (C) 2002-2005 Tommi Saviranta <tsaviran@cs.helsinki.fi>
  *	(C) 1998-2002 Sebastian Kienzl <zap@riot.org>
  * -------------------------------------------------------
  * This program is free software; you can redistribute it and/or modify
@@ -81,9 +81,18 @@ is_perm(
 {
 	llist_node	*node;
 	int		allowed = 0;
+	char		*host;
+
+	/* Set hostname if it is known. */
+	if (status.goodhostname != 0) {
+		host = status.idhostname + status.goodhostname;
+	} else {
+		host = NULL;
+	}
 
 	for (node = list->list.head; node != NULL; node = node->next) {
-		if (match(name, ((perm_type *) node->data)->name)) {
+		if (match_replace(name, ((perm_type *) node->data)->name,
+					"@@", 2, host) == 1) {
 			allowed = ((perm_type *) node->data)->allowed;
 		}
 	}
